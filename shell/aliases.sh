5dca0a30d34e0f210cf5fb3ec820fca728c3daab
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-03-31 01:54:29 +0200

    Add alias to shell.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,4 @@
-alias d='cd'
+alias c='cd'
 
 # Aliases to protect against overwriting
 alias cp='cp -i'
@@ -7,3 +7,4 @@ alias mv='mv -i'
 # Editors
 alias v='nvim'
 alias e='emacsclient --no-wait'
+alias gll='git log --graph --pretty=oneline --abbrev-commit'
