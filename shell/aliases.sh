28dd57c25c4668b71a64bff5f4fd983fdc68a8d5
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-04-14 18:26:28 +0200

    Update aliases.sh

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -12,6 +12,13 @@ alias f='fdfind'
 
 
 #directories
-alias do='cd ~/.dotfiles'
+#alias do='cd ~/.dotfiles' this line caused a tone of problems for me.
 alias regi3='nvim ~/.dotfiles/config/regolith/i3/config'
 alias ozsh='cd ~/.dotfiles/zsh/oh-my-zsh'
+
+#miscelanous or recently added.
+alias .d='cd /home/tassilo/.dotfiles/'
+alias ashell='nvim /home/tassilo/.dotfiles/shell/aliases.sh'
+alias hm='xdg-open semester2/Höhere\ Mathematik\ I\ \(Analysis\)\ für\ die\ Fachrichtung\ Informatik/Vorlesungsmaterial/Skript\ Höhere\ Mathematik\ 1.pdf' 
+alias hm1='xdg-open semester2/Höhere Mathematik I (Analysis) für die Fachrichtung Informatik/Vorlesungsmaterial/Skript Höhere Mathematik 1.pdf'
+alias rs='cd semester; ranger'
