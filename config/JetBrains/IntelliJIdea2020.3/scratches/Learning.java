d875d5e09730470ec43e9430c7c45f2015494553
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-06-06 23:17:05 +0200

    Add JetBrains, discord and vlc configuration files.

---
+++ config/JetBrains/IntelliJIdea2020.3/scratches/Learning.java
@@ -0,0 +1,5 @@
+class DuplicateDemo {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
---
+++ config/JetBrains/IntelliJIdea2021.1/scratches/Learning.java
@@ -0,0 +1,9 @@
+import javafx.scene;
+class DuplicateDemo {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        javafx.scene =
+    }
+
+
+}
