bc1fe02933fc65a56e26a2ce9a64495d5108d553
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-07-22 12:03:26 +0200

    add Screenlayout scripts from arandr

---
+++ screenlayout/doublePort.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --pos 0x502 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 1920x1080 --pos 1920x0 --rotate left --output DisplayPort-3 --mode 1920x1080 --pos 3000x0 --rotate right
