ef6260525904edd164141b23bc96b08de94bae50
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-07-22 12:03:26 +0200

    add Screenlayout scripts from arandr

---
+++ screenlayout/tripleLand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --pos 0x502 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 1920x1080 --pos 1920x502 --rotate normal --output DisplayPort-3 --mode 1920x1080 --pos 3840x502 --rotate normal
