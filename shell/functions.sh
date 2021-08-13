8e843138ff5325cc399835a77b75c547cf2df804
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-08-13 16:19:51 +0200

    Add function apt-history

--- shell/functions.sh
+++ shell/functions.sh
@@ -11,3 +11,23 @@ path_prepend() {
     path_remove "$1"
     PATH="$1${PATH:+":$PATH"}"
 }
+function apt-history(){
+      case "$1" in
+        install)
+              cat /var/log/dpkg.log | grep 'install '
+              ;;
+        upgrade|remove)
+              cat /var/log/dpkg.log | grep $1
+              ;;
+        rollback)
+              cat /var/log/dpkg.log | grep upgrade | \
+                  grep "$2" -A10000000 | \
+                  grep "$3" -B10000000 | \
+                  awk '{print $4"="$5}'
+              ;;
+        *)
+              cat /var/log/dpkg.log
+              ;;
+      esac
+}
+
