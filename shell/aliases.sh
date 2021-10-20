1b24ffd57ccede3bc7099bc3103e6704b410233d
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-10-20 15:46:52 +0200

    Update aliases for third semester

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,4 @@
-unifiles="$HOME/Dropbox/semester2"
+unifiles="$HOME/Dropbox/semester3"
 swtfiles="$unifiles/Softwaretechnik\ I\ mit\ Übung/"
 lafiles="$unifiles/Lineare\ Algebra\ \ II\ für\ Mathematik/"
 algofiles="$unifiles/Algorithmen\ I\ \(SS\ 2021\)/"
@@ -24,7 +24,7 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias ainstall='sudo apt-get update -y & sudo apt-get upgrade -y && sudo apt-get install'
 
-#directories
+#directories and files
 #alias do='cd ~/.dotfiles' this line caused a tone of problems for me.
 alias regi3='nvim ~/.dotfiles/config/regolith/i3/config'
 alias aalias='nvim ~/.dotfiles/shell/aliases.sh'
@@ -34,12 +34,10 @@ alias .d='cd /home/tassilo/.dotfiles/'
 alias ashell="nvim /home/tassilo/.dotfiles/shell/aliases.sh"
 alias dmemacs="~/.local/lib/python3.9/site-packages/memacs/"
 
-########################################
 # Uni Files
-########################################
-
 alias hm1="xdg-open $unifiles/Höhere\ Mathematik\ I\ \(Analysis\)\ für\ die\ Fachrichtung\ Informatik/Vorlesungsmaterial/Skript\ Höhere\ Mathematik\ 1.pdf && exit" 
 alias hm2="xdg-open $unifiles/Höhere\ Mathematik\ I\ \(Analysis\)\ für\ die\ Fachrichtung\ Informatik/Vorlesungsmaterial/HM2.pdf && exit" 
+
 
 #navigate to ranger
 alias rs="cd $unifiles; ranger"
@@ -48,35 +46,12 @@ alias la="cd $lafiles; ranger"
 alias swt="cd $swtfiles; ranger"
 alias dgt="cd $dgtfiles; ranger"
 alias algo="cd $algofiles; ranger"
+alias ot="cd $HOME/repos/oTree/; ranger"
+
 
 alias la1="xdg-open semester2/Lineare\ Algebra\ II\ für\ Mathematik/stuff/LA1.pdf && exit"
 alias dIlias='exec ~/Installations/KIT-ILIAS-downloader -t -o ~/Dropbox/semester2'
 alias update-alternatives='sudo update-alternatives'
 
 #miscelanous or recently added.
-
-
-sswt() {
-	pdfgrep -Ri $1 "$swtfiles"
-	grep -Ri $1 "$swtfiles"
-}
-sla() {
-	pdfgrep -Ri $1 "$lafiles"
-
-	grep -Ri $1 "$lafiles"
-}
-salgo() {
-	pdfgrep -Ri $1 "$algofiles"
-	grep -Ri $1 "$algofiles"
-}
-sdgt() {
-	pdfgrep -Ri $1 "$dgtfiles"
-	grep -Ri $1 "$dgtfiles"
-}
-shm() {
-	pdfgrep -Ri $1 "$hmfiles"
-	grep -Ri $1 "$hmfiles"
-}
-
-
 alias vpn='sudo openvpn --config ~/Downloads/kit.ovpn'
