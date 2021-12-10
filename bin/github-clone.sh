02fee97cf2f31e756702986bb13a61b4001b0069
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-12-10 22:14:51 +0100

    Add github-clone

---
+++ bin/github-clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :github-clone.sh
+#description    :Use git clone with ssh instead of http address
+#author         :Tassilo Neubauer
+#date           :20211208
+#version        :0.1    
+#usage          :./github-clone.sh
+#notes          :       
+#bash_version   :5.1.4(1)-release
+#============================================================================
+
+echo "$1" | sed 's/.*github.com\//git@github.com:/' | sed 's/$/.git/' | xargs git clone
