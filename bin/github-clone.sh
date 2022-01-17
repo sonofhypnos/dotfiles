2be5d7014a522bd3dfdee872e24b912b330279a6
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-01-17 23:37:38 +0100

    Update github-clone.sh

--- bin/github-clone.sh
+++ bin/github-clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -   
 #title          :github-clone.sh
-#description    :Use git clone with ssh instead of http address
+#description    :Use git clone with ssh instead of http address. Use 
 #author         :Tassilo Neubauer
 #date           :20211208
 #version        :0.1    
@@ -8,5 +8,19 @@
 #notes          :       
 #bash_version   :5.1.4(1)-release
 #============================================================================
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)" #https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
 
-echo "$1" | sed 's/.*github.com\//git@github.com:/' | sed 's/$/.git/' | xargs git clone --recurse-submodules
+if [ "$inside_git_repo" ]; then #checks if inside git repository: 
+	read -p "You are inside a git repository. Github-clone will create a new submodule for you. Are you sure you want to continue?" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Downloading and Installing git submodule"
+	echo "$1" | sed 's/.*github.com\//git@github.com:/' | sed 's/$/.git/' | xargs git submodule add
+	git submodule update --init --recursive
+	fi
+else
+	echo "Downloading and installing git repository"
+	echo "$1" | sed 's/.*github.com\//git@github.com:/' | sed 's/$/.git/' | xargs git clone --recurse-submodules
+fi;
+
+
