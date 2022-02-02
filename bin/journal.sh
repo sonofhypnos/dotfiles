7891bdc319dbe2bfa8a33d78774621bfe09cfdaf
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-02-02 20:50:11 +0100

    Add journal command

---
+++ bin/journal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -   
+#title          :journal.sh
+#description    :Capture Journal Entry in Emacs
+#author         :Tassilo Neubauer
+#date           :20220121
+#version        :0.1    
+#usage          :./journal.sh
+#notes          :       
+#bash_version   :5.1.4(1)-release
+#============================================================================
+
+/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e "(org-roam-dailies-capture-today)" &
+sleep 0.3
+i3-msg '[title="_emacs scratchpad_"] scratchpad show'
