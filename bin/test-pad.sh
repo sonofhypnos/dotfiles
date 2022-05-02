e59cbd6c1abc13715db977cb5066126e64f9b59f
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-05-02 13:52:35 +0200

    Add test-pad to test capture without needing to restart the server

---
+++ bin/test-pad.sh
@@ -0,0 +1,6 @@
+#!/bin/env sh
+
+# launch emacsclient with special frame name
+/usr/local/bin/emacs -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" "$@"
+#i3-msg '[title="_emacs scratchpad_"] move scratchpad' #this might be superflous, because this is already done with all windows of this type (it might also need some deference?"
+i3-msg '[title="_emacs scratchpad_"] scratchpad show' 
