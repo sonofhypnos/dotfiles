27508737fba95e948d92adc0bc8630da1015a914
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-05-05 12:55:15 +0200

    Configure capture for emacs

--- bin/capture.sh
+++ bin/capture.sh
@@ -2,6 +2,5 @@
 
 # launch emacsclient with special frame name
 # TODO Figure out why sleep is necessary here?
-/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e "(org-capture)" &
-sleep 0.3
+/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
 i3-msg '[title="_emacs scratchpad_"] scratchpad show' 
--- bin/journal.sh
+++ bin/journal.sh
@@ -9,6 +9,5 @@
 #bash_version   :5.1.4(1)-release
 #============================================================================
 
-/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e "(org-roam-dailies-capture-today)" &
-sleep 0.3
+/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e "(org-roam-dailies-capture-today)"
 i3-msg '[title="_emacs scratchpad_"] scratchpad show'
