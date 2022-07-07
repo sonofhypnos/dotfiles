128e735e790fb45c8c7248bc0f7a753dedbc1748
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-07-07 13:26:36 +0200

    Update capture to support doom capture

--- bin/capture.sh
+++ bin/capture.sh
@@ -2,5 +2,9 @@
 
 # launch emacsclient with special frame name
 # TODO Figure out why sleep is necessary here?
-/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
-i3-msg '[title="_emacs scratchpad_"] scratchpad show' 
+#/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
+org-capture "$@"
+i3-msg '[title="doom-capture"] move scratchpad'
+i3-msg '[title="doom-capture"] scratchpad show'
+i3-msg '[title="_emacs scratchpad_"] scratchpad show'
+
