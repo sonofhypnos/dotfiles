68195b1b015cf2eaa33e0c2f6cc67dafc1d90aec
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-09-05 15:53:24 +0200

    Update capture script

--- bin/capture.sh
+++ bin/capture.sh
@@ -1,10 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# launch emacsclient with special frame name.
+# works better than org-capture from doom
+set -e
+/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
 
-# launch emacsclient with special frame name
-# TODO Figure out why sleep is necessary here?
-#/usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
-org-capture "$@"
-i3-msg '[title="doom-capture"] move scratchpad'
-i3-msg '[title="doom-capture"] scratchpad show'
 i3-msg '[title="_emacs scratchpad_"] scratchpad show'
 
+#make_visible (){
+##    wid="$(xdotool search --classname "${name}" | tail -1 2> /dev/null)"
+##    echo $(expr length "${wid}")
+#    while ! i3-msg "[title=\"doom-capture\"] scratchpad show"; do
+#	sleep 0.05;
+#    done
+#}
+## Wait for application to be available
+#cleanup(){
+#    org-capture "$@"
+##i3-msg '[title="doom-capture"] move scratchpad'
+##        i3-msg '[title="doom-capture"] floating disable'
+##        i3-msg '[title="doom-capture"] floating enable'
+#}
+#
+#make_visible & cleanup "$@"
+
+# i3-msg '[title="doom-capture"] move scratchpad'
+# i3-msg '[title="_emacs scratchpad_"] scratchpad show'
