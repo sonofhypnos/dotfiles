ef4f89d53a284b103167c39ccc5ea6e518e2e911
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-09-05 15:53:15 +0200

    Update backup script

--- bin/backup_data.sh
+++ bin/backup_data.sh
@@ -1,62 +1,62 @@
-#!/bin/bash -   
+#!/bin/bash -
 #title          :backup_data.sh
 #description    :Backup data from laptop to pc
 #author         :Tassilo Neubauer
 #date           :20220102
-#version        :0.1    
+#version        :0.1
 #usage          :./backup_data.sh
-#notes          :       
+#notes          :
 #bash_version   :5.1.4(1)-release
 #============================================================================
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
-if cat /etc/fstab | grep -q "UUID=8e97e9d3-ec0b-4426-8627-8f994e2f94c2 /media/tassilo/backup"; then
-	echo "/etc/fstab seems configured"
-else
-	echo "diretory /etc/fstab must be edited. Probably add: \"UUID=8e97e9d3-ec0b-4426-8627-8f994e2f94c2 /media/tassilo/backup ext4 noauto 0 2\""
-	exit
-fi
-
-
-mount /media/tassilo/backup
 # Setting this, so the repo does not need to be given on the commandline:
 #
-export BORG_REPO='/media/tassilo/backup/borgE15/'
+#export BORG_REPO='/media/tassilo/backup/borgE15/'
+export BORG_REPO='tassilo@nixos:/home/tassilo/backups'
 
 # See the section "Passphrase notes" for more infos.
 #export BORG_PASSPHRASE='XYZl0ngandsecurepa_55_phrasea&&123'
 
 # some helpers and error handling:
-info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+info() { printf "\n%s %s\n\n" "$(date)" "$*" >&2; }
 trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
 
 info "Starting backup"
 
 # Backup the most important directories into an archive named after
 # the machine this script is currently running on:
+# TODO remove emacs stuff from backup
 
-borg create                            \
-    --verbose                          \
-    --filter AME                       \
-    --list                             \
-    --stats                            \
-    --show-rc                          \
-    --exclude-caches                   \
-    --exclude '/home/*/.cache/*'       \
-    --exclude '/home/*/CacheStorage/*' \
-    --exclude '/home/*/CacheStorage/*' \
-    --exclude '/var/tmp/*'             \
-    --exclude '/home/tassilo/Dropbox/semester*/**/*.mp4'           \
-                                       \
-    ::'{hostname}-{now}'               \
-    /etc                               \
-    /home                              \
-    /root                              \
-    /var                               \
+borg create \
+    --verbose \
+    --filter AME \
+    --list \
+    --stats \
+    --show-rc \
+    --exclude-caches \
+    --exclude '/home/**/.cache/**' \
+    --exclude '/home/**/.Cache/**' \
+    --exclude '/home/**/cache/**' \
+    --exclude '/home/**/Cache/**' \
+    --exclude '/home/**/CacheStorage/**' \
+    --exclude '/home/**/CacheStorage/**' \
+    --exclude '/home/**/CachedData/**' \
+    --exclude '/home/tassilo/Dropbox/**' \
+    --exclude '/home/**/Code Cache/**' \
+    --exclude '/var/snap/lxd/common/lxd/disks/storage1.img' \
+    --exclude '/var/tmp/*' \
+    --exclude '/home/tassilo/Dropbox/semester*/**/*.mp4' \
+    \
+    ::'{hostname}-{now}' \
+    /etc \
+    /home \
+    /root \
+    /var
 
 backup_exit=$?
 
@@ -67,18 +67,18 @@ info "Pruning repository"
 # limit prune's operation to this machine's archives and not apply to
 # other machines' archives also:
 
-borg prune                          \
-    --list                          \
-    --prefix '{hostname}-'          \
-    --show-rc                       \
-    --keep-daily    7               \
-    --keep-weekly   4               \
-    --keep-monthly  6               \
+borg prune \
+    --list \
+    --prefix '{hostname}-' \
+    --show-rc \
+    --keep-daily 7 \
+    --keep-weekly 4 \
+    --keep-monthly 6
 
 prune_exit=$?
 
 # use highest exit code as global exit code
-global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+global_exit=$((backup_exit > prune_exit ? backup_exit : prune_exit))
 
 if [ ${global_exit} -eq 0 ]; then
     info "Backup and Prune finished successfully"
@@ -95,5 +95,3 @@ fi
 exit ${global_exit}
 
 #do borg backup stuff, if the thing is actually mounted otherwise inform that
-
-umount /media/tassilo/backup
