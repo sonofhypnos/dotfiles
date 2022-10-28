9c99189c861dae10fef1eca03d6b616d3fbbfd38
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-10-28 14:09:22 +0200

    Hardcode i3-msg path
    Not sure why I did this. Might be broken.

--- bin/capture.sh
+++ bin/capture.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 # launch emacsclient with special frame name.
 # works better than org-capture from doom
 set -e
 /usr/local/bin/emacsclient -n -c  --frame-parameters "((name . \"_emacs scratchpad_\"))" -e '(org-capture :entry "n")' #sleep is added so the program realizes it was successful?
 
-i3-msg '[title="_emacs scratchpad_"] scratchpad show'
+/usr/bin/i3-msg '[title="_emacs scratchpad_"] scratchpad show'
 
 #make_visible (){
 ##    wid="$(xdotool search --classname "${name}" | tail -1 2> /dev/null)"
