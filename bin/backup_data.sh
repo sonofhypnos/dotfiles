64ccf3b9a0a60f0336e318770cbe7cc7aa8f231f
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2023-04-13 19:19:36 +1000

    Update backup to use password manager
    I have not tested this actually works. If it doesn't I'll update this soon.

--- bin/backup_data.sh
+++ bin/backup_data.sh
@@ -19,7 +19,8 @@ fi
 #export BORG_REPO='/media/tassilo/backup/borgE15/'
 
 # See the section "Passphrase notes" for more infos.
-#export BORG_PASSPHRASE='XYZl0ngandsecurepa_55_phrasea&&123'
+op whoami || eval $(op signin) #check if logged in with op whoami
+export BORG_PASSPHRASE=$(op read "op://Personal/Encryption borg base laptop passphrase/password")
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$(date)" "$*" >&2; }
@@ -29,26 +30,19 @@ info() { printf "\n%s %s\n\n" "$(date)" "$*" >&2; }
 #likely to you currently!
 #borg break-lock root@nixos:/home/tassilo/backups
 
-info "Starting backup on nix server"
-export BORG_REPO='root@nixos:/home/tassilo/backups'
-
-
-
+info "Starting backup with borg base server"
+#export BORG_REPO='root@nixos:/home/tassilo/backups'
+export BORG_REPO=ssh://d7h5sb0u@borgbase/./repo
 
 #backup_repo{
 trap 'echo $( date ) Backup interrupted >&2; notify-send -u critical "Backup failed!" ; exit 2' INT TERM
 
-
-
-
-
 info "Starting backup"
-
 
 # Backup the most important directories into an archive named after
 # the machine this script is currently running on:
 # TODO remove emacs stuff from backup
-#TODO: further things to exclude: 
+#TODO: further things to exclude:
 # straight compiled emacs things
 # /var/cache...
 # TODO: either empty the trash or exclude it in your backup?
@@ -119,7 +113,6 @@ fi
 
 #} #end of backup_repo
 
-
 #do borg backup stuff, if the thing is actually mounted otherwise inform that
 #TODO using global variable instead of parameter is ugly, make this pretty once I trust my bash skills
 
@@ -133,6 +126,70 @@ fi
 #backup_repo
 #info "backup finished family server"
 
+info "Starting backup with nix server"
+
+export BORG_REPO='root@nixos:/home/tassilo/backups'
+
+borg create \
+    --verbose \
+    --filter AME \
+    --list \
+    --stats \
+    --show-rc \
+    --exclude-caches \
+    --exclude '/home/**/.cache/**' \
+    --exclude '/home/**/.Cache/**' \
+    --exclude '/home/**/cache/**' \
+    --exclude '/home/**/Cache/**' \
+    --exclude '/home/**/CacheStorage/**' \
+    --exclude '/home/**/CacheStorage/**' \
+    --exclude '/home/**/CachedData/**' \
+    --exclude '/home/tassilo/Dropbox/**' \
+    --exclude '/home/tassilo/Games/' \
+    --exclude '/home/**/Code Cache/**' \
+    --exclude '/var/snap/lxd/common/lxd/disks/storage1.img' \
+    --exclude '/var/tmp/*' \
+    --exclude '/home/tassilo/Dropbox/semester*/**/*.mp4' \
+    \
+    ::'{hostname}-{now}' \
+    /etc \
+    /home \
+    /root \
+    /var
+
+backup_exit=$?
+
+info "Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-' prefix is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+borg prune \
+    --list \
+    --prefix '{hostname}-' \
+    --show-rc \
+    --keep-daily 7 \
+    --keep-weekly 4 \
+    --keep-monthly 6
+
+prune_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$((backup_exit > prune_exit ? backup_exit : prune_exit))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Backup 2 and Prune finished successfully"
+    logger "Backup 2 and Prune finished successfully"
+
+elif [ ${global_exit} -eq 1 ]; then
+    info "Backup 2 and/or Prune finished with warnings"
+    logger -p user.warn "Backup 2 and/or Prune finished with warnings"
+else
+    info "Backup and/or Prune finished with errors"
+    logger -p user.error "Backup 2 and/or Prune finished with errors"
+fi
 
 info "finished backup"
 
