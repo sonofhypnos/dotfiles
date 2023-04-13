ab27432d4656284d129012dc800cb56854610af7
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2023-04-13 19:22:28 +1000

    Remove 1password from nix again.
    Did not work out. 1password was somehow not able to connect.

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  pkgsUnstable = import <nixpkgs-unstable> {
+    config.allowUnfree = true;
+  }; # This line allows me to selectively use unstable version (it also enables unfree packages in this version)
+
+in {
+
   home.username = "tassilo";
   home.homeDirectory = "/home/tassilo";
   # This value determines the Home Manager release that your
@@ -12,14 +18,18 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "22.05";
+
   # nixpkgs.overlays = [
   #  (import (builtins.fetchTarball {
   #    url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
   # }))
   #  ];
 
-  nixpkgs.config.allowUnfree = true; # needed for 1password
-  home.packages = with pkgs; [ ripgrep _1password ];
+  home.packages = [
+    pkgs.ripgrep
+    #pkgsUnstable._1password #NOTE: not installing 1password for now since connection between apps did not work
+    #pkgsUnstable._1password-gui
+  ];
 
   #programs.emacs = {
   #    enable = true;
