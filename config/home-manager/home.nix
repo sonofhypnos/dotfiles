02132c775b1d79ec2e0ea2dff88b354b29d4d605
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2023-09-01 19:39:33 +0200

    Cleanup home.nix

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -23,12 +23,6 @@ in {
   # changes in each release.
   home.stateVersion = "23.05";
 
-  # nixpkgs.overlays = [
-  #  (import (builtins.fetchTarball {
-  #    url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-  # }))
-  #  ];
-
   home.packages = [
     pkgs.ripgrep
     pkgs.zathura
@@ -37,34 +31,18 @@ in {
     pkgs.xclip
     pkgs.git-lfs
     pkgs.git
-    # pkgs.bfg-repo-cleaner
     pkgs.git-filter-repo
-    # pkgsUnstable.lua54Packages.digestif
-    # (pkgs.rxvt_unicode.override {
-    #   withPerls = [ pkgs.rxvt_unicode_perl ]; # Add copy and paste to urxvt
-    # })
-    #pkgsUnstable._1password #NOTE: not installing 1password for now since connection between apps did not work
-    #pkgsUnstable._1password-gui
+    pkgs.emacs29
+    pkgs.texlab # for emacs lsp in tex
   ];
 
-  # Configuration for URxvt to use the Perl extension
-  # xsession.terminal = {
-  #   urxvt = {
-  #     perl-lib = "${
-  #         ./.
-  #       }/urxvt_clipboard.pl"; # This line references the script in the current directory
-  #   };
-  # };
   home.file.".Xresources".text = ''
-    ${xresourcesContent}  # Include content from Xresources
+    ${xresourcesContent}
 
     URxvt.perl-lib: ${clipboardScriptPath}
     URxvt.perl-ext-common: default,clipboard
     URxvt.keysym.C-S-V: perl:clipboard:paste
   '';
-  #In the above not sure what the line:
-  #URxvt.perl-ext-common: default,clipboard
-  #does exactly
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
