a374c123a764aa781073620e14ded48f634e2a77
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2023-09-02 02:26:34 +0200

    Update zsh config

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -49,28 +49,27 @@ in {
     '';
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "sudo" ]; # Add your plugins here
+      plugins = [
+        "git"
+        "alias-finder"
+        "colored-man-pages"
+        "colorize"
+        "fasd"
+        "zsh-syntax-highlighting"
+        "zsh-autosuggestions"
+        "zsh-wakatime"
+      ]; # Add your plugins here
     };
-    plugins = [
-      # "git"
-      # "alias-finder"
-      # "colored-man-pages"
-      # "colorize"
-      # "fasd"
-      # "zsh-syntax-highlighting"
-      # "zsh-autosuggestions"
-      # "zsh-wakatime"
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.7.0";
-          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
-        };
-      }
-    ];
+    plugins = [{
+      name = "zsh-nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = pkgs.fetchFromGitHub {
+        owner = "chisui";
+        repo = "zsh-nix-shell";
+        rev = "v0.7.0";
+        sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+      };
+    }];
   };
 
   home.file.".Xresources".text = ''
