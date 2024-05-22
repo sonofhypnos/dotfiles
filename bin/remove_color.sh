a15225f64fbcefae034a3820190afd96b6916149
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-05-22 13:55:46 +0200

    Add remove_color script

---
+++ bin/remove_color.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :remove_color.sh
+#description    :removes color from output
+#author         :Tassilo Neubauer
+#date           :20240522
+#version        :0.1    
+#usage          :./remove_color.sh
+#notes          :       
+#bash_version   :5.1.16(1)-release
+#============================================================================
+
+sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"
