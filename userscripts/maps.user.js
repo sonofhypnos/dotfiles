a914a9ba1bd9b6a22c770cf28b7706ab6b403710
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-11-19 16:27:42 +0100

    Add maps userscript

---
+++ userscripts/maps.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Maps Shortcuts
+// @namespace    Smart User Scripts
+// @version      0.2
+// @description  Shortcut for maps
+// @match        https://maps.google.com/*
+// @grant        none
+// @author      Tassilo Neubauer
+// @updateURL   https://raw.githubusercontent.com/sonofhypnos/dotfiles/main/userscripts/maps.user.js
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === '/' && !isInputFocused()) {
+            e.preventDefault();
+            const searchInput = document.querySelector('#searchboxinput');
+            if (searchInput) {
+                searchInput.focus();
+                searchInput.select();
+            }
+        }
+    });
+
+    function isInputFocused() {
+        const activeElement = document.activeElement;
+        return activeElement.tagName === 'INPUT' ||
+               activeElement.tagName === 'TEXTAREA' ||
+               activeElement.isContentEditable;
+    }
+})();
