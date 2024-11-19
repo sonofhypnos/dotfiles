34f206c2e07a253d802778666ca79c5b18b46ba0
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-11-19 16:17:30 +0100

    Add evening routine scripts

---
+++ bin/evening_routine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :evening_routine.sh
+#description    :Starts evening routine script
+#author         :Tassilo Neubauer
+#date           :20241115
+#version        :0.1    
+#usage          :./evening_routine.sh
+#notes          :       
+#bash_version   :5.1.16(1)-release
+#============================================================================
+
+/usr/local/bin/emacsclient -n -c -a "" --frame-parameters "((name . \"_emacs scratchpad_\"))" -e "(org-roam-dailies-goto-today)" && i3-msg '[title="_emacs scratchpad_"] scratchpad show'
