213507a72e3a74a9d41d6d7001adef90b62e1411
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2025-02-28 19:51:04 +1100

    Format emacs-overlay file

--- nix/emacs-overlay.nix
+++ nix/emacs-overlay.nix
@@ -1,12 +1,14 @@
 with rec {
-  emacs-overlay = import (builtins.fetchTarball { url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz; });
+  emacs-overlay = import (builtins.fetchTarball {
+    url =
+      "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+  });
   pkgs = import <nixpkgs> { overlays = [ emacs-overlay ]; };
 };
 
 rec {
-  emacs-head = pkgs.emacsGcc.overrideAttrs ( old: {
-    configureFlags = old.configureFlags ++ [ "--with-json"  ];
-  });
+  emacs-head = pkgs.emacsGcc.overrideAttrs
+    (old: { configureFlags = old.configureFlags ++ [ "--with-json" ]; });
   emacs-head-nox = emacs-head.override {
     withX = false;
     withGTK2 = false;
