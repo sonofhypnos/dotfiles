e92db08f982fef653ac369ec7603f154aaee081b
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2025-02-28 19:48:06 +1100

    Add doom profiles

---
+++ config/doom/profiles.el
@@ -0,0 +1,14 @@
+;; -*- mode: emacs-lisp; -*-
+((emacs29  ; Default profile for Emacs 29.1
+  (user-emacs-directory . "~/.emacs.d/")
+  (doom-user-dir . "~/.doom.d/")
+  ("DOOMDIR" . "~/.doom.d/")
+  ("EMACS_BIN" . "emacs-29.1")
+  ;; Set this as the default profile
+  (is-default . t))
+
+ (emacs30  ; Profile for Emacs 30
+  (user-emacs-directory . "~/.emacs30.d/")
+  (doom-user-dir . "~/.doom30.d/")
+  ("DOOMDIR" . "~/.doom30.d/")
+  ("EMACS_BIN" . "emacs-30")))
