67f58f90cf827012c14f074d3b34de7277407602
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2025-03-16 11:49:41 +1100

    Update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,12 +1,3 @@
-unifiles2="$HOME/Dropbox/semester2"
-#unifiles="$HOME/Dropbox/semester3"
-unifiles="$HOME/Dropbox/semester6"
-unifiles5="$HOME/Dropbox/semester5"
-unifiles6="$HOME/Dropbox/semester6"
-mphfiles="$unifiles/4040451\ –\ Moderne\ Physik\ für\ Informatiker"
-cvxfiles="$unifiles5/convex_optimization/"
-pgfiles="$unifiles6/24614\ –\ Algorithmen\ für\ planare\ Graphen\ \(mit\ Übungen\)"
-
 #convenience
 alias d='cd'
 alias p='popd'
@@ -45,19 +36,6 @@ alias ashell="vim ~/.dotfiles/shell/aliases.sh"
 alias dmemacs="~/.local/lib/python3.9/site-packages/memacs/"
 
 # Uni Files
-# alias hm1="xdg-open ~/Dropbox/semester2/Höhere\ Mathematik\ I\ \(Analysis\)\ für\ die\ Fachrichtung\ Informatik/Vorlesungsmaterial/Skript\ Höhere\ Mathematik\ 1.pdf && exit"
-# alias hm2="xdg-open $unifiles2/Höhere\ Mathematik\ I\ \(Analysis\)\ für\ die\ Fachrichtung\ Informatik/Vorlesungsmaterial/HM2.pdf && exit"
-
-# alias rs="cd $unifiles ; ranger"
-# alias mph="xdg-open $mphfiles/Vorlesung-Skript/VL.pdf  ; ranger"
-# alias cvx="cd $cvxfiles ; ranger"
-# alias txt="cd ~/Dropbox/textbooks/ ; ranger"
-# alias ml="zathura ~/Dropbox/textbooks/machine\ learning\ from\ theory\ to\ algorithms.pdf"
-# alias pg="cd $pgfiles ; ranger"
-
-# alias la1="xdg-open semester2/Lineare\ Algebra\ II\ für\ Mathematik/stuff/LA1.pdf && exit"
-# alias dIlias='exec ~/Installations/KIT-ILIAS-downloader -t -o ~/Dropbox/semester2'
-# alias update-alternatives='sudo update-alternatives'
 alias int="cd ~/Dropbox/DokumenteTassilo/intelligenceAmplification/reading/; ranger"
 
 #miscelanous or recently added.
@@ -65,5 +43,5 @@ alias vpn="sudo $HOME/vpnstuff"
 alias htop="sudo htop"
 alias abox="archivebox"
 gitdownload() {
-	curl -L $(echo "$1" | sed -r 's/https:\/\/github\.com\/(.*)$/git@github.com:\1\/archive\/master.tar.gz/') | tar zxf -
+    curl -L $(echo "$1" | sed -r 's/https:\/\/github\.com\/(.*)$/git@github.com:\1\/archive\/master.tar.gz/') | tar zxf -
 }
